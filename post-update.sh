190271280716092f4fa1b4fa2425ecb538af4f41
vessokolev <vesso.kolev@gmail.com>
2019-07-16 16:40:27 -0700

    new file/dir topology

--- post-update.sh
+++ post-update.sh
@@ -4,7 +4,7 @@ sed -i 's/installonly_limit=5/installonly_limit=2/' /etc/yum.conf
 sed -i 's/apply_updates = no/apply_updates = yes/g' /etc/yum/yum-cron.conf
 echo -e "131.188.3.221\n193.67.79.202" > /etc/ntp/step-tickers
 systemctl stop chronyd
-wget https://raw.githubusercontent.com/vessokolev/vm-install/master/chrony.conf -O /etc/chrony.conf
+wget https://raw.githubusercontent.com/vessokolev/vm-install/master/etc/chrony.conf -O /etc/chrony.conf
 systemctl start chronyd
 mkdir /root/.ssh
 chmod 700 /root/.ssh
