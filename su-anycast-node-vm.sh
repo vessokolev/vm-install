c9578d7f99378137f24f1fc4174ee90f1425d261
vessokolev <vesso.kolev@gmail.com>
2019-07-16 15:24:05 -0700

    correct location of ISO file

--- su-anycast-node-vm.sh
+++ su-anycast-node-vm.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-virt-install --name centos-7-template --memory=1024 --vcpus=1 --os-type linux --location=CentOS-7-x86_64-Minimal-1810.iso --disk=/var/lib/libvirt/images/centos-7-template.qcow2,size=26843545600 --network=bridge:virbr0,model=e1000 --graphics=none --os-variant=centos7.0 --console pty,target_type=serial -x 'console=ttyS0,115200n8 serial' -x "ks=https://raw.githubusercontent.com/vessokolev/vm-install/master/su-anycast-node-vm.cfg"
-
+virt-install --name centos-7-template --memory=1024 --vcpus=1 --os-type linux --location=/var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1810.iso --disk=/var/lib/libvirt/images/centos-7-template.qcow2,size=25 --network=bridge:virbr0,model=e1000 --graphics=none --os-variant=centos7.0 --console pty,target_type=serial -x 'console=ttyS0,115200n8 serial' -x "ks=https://raw.githubusercontent.com/vessokolev/vm-install/master/su-anycast-node-vm.cfg"
